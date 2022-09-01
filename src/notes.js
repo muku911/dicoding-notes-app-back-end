1e33eed192280ff38bb0e751bf566e4bd89f520f
muku911 <husni@nfbsbogor.sch.id>
2022-09-01 20:24:37 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+// {
+//     id: string,
+//     title: string,
+//     createdAt: string,
+//     updatedAt: string,
+//     tags: array of string,
+//     body: string,
+//    }
+
+const notes = [];
+
+module.exports = notes;
